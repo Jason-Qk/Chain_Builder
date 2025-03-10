59b58a2265e954d0afa14b900603a328ddf2488f
Jason J.S. Quek <jason.js.quek@gmail.com>
2025-03-10 12:40:19 +1100

    Import Maya modules

---
+++ chain_builder.py
@@ -0,0 +1,10 @@
+# Designed to run within Maya, this script generates a window that lets users create chain-like objects
+
+import maya.cmds as cmds
+import MASH.api as mapi
+
+# ============================== FUNCTION LIBRARY ==============================
+
+
+# ================================ MAIN PROGRAM ================================
+
