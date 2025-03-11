65d804630f7f9f36fff0e755fbfaab497b8459f9
Jason J.S. Quek <jason.js.quek@gmail.com>
2025-03-11 23:19:23 +1100

    Create dropdown menu to select chain link

--- chain_builder.py
+++ chain_builder.py
@@ -18,6 +18,32 @@ if cmds.window(__WINDOW__, exists = True):
 # Create window
 cmds.window(__WINDOW__, title = "Chain Builder", widthHeight = [500, 300])
 
+# Construct controls
+layout_outermost = cmds.columnLayout("layout_outermost", adjustableColumn = True)
+
+cmds.text("Window Title", font = "boldLabelFont", height = 50) # Title to be replaced by image title
+
+cmds.separator(height=10, style = "shelf")
+
+# Select object to serve as chain link
+
+# cmds.text("Select Chain Link Object: ", font = "boldLabelFont", align = "left", height = 30)
+chain_link_choices = cmds.optionMenu(label = "Chain Link Object: ")
+
+
+# layout_chain_link = cmds.rowLayout("layout_chain_link", numberOfColumns = 2, adjustableColumn = 2)
+
+cmds.setParent(layout_outermost)
+cmds.separator(height=10, style = "shelf")
+
+# TODO: Define chain attributes
+# - Direction of duplication - x/y/z axis
+# - Pattern variation e.g. rotate every 2nd link 90 degrees
+
+# TODO: Generate chain group then merge mesh
+
+# TODO: Curve warp chain group onto a selected curve
+
 
 # Display window
 cmds.showWindow(__WINDOW__)
