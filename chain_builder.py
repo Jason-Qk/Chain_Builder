ff49272fe21cde1e60d1489d31a8d7496263b2d5
Jason J.S. Quek <jason.js.quek@gmail.com>
2025-03-11 15:54:05 +1100

    Generate GUI within Maya

--- chain_builder.py
+++ chain_builder.py
@@ -3,8 +3,21 @@
 import maya.cmds as cmds
 import MASH.api as mapi
 
+__WINDOW__ = "chain_builder_UI"
+
 # ============================== FUNCTION LIBRARY ==============================
 
 
 # ================================ MAIN PROGRAM ================================
 
+# Close window if currently open including clearing changes to window dimensions
+if cmds.window(__WINDOW__, exists = True):
+    cmds.deleteUI(__WINDOW__)
+    cmds.windowPref(__WINDOW__, remove=True)
+
+# Create window
+cmds.window(__WINDOW__, title = "Chain Builder", widthHeight = [500, 300])
+
+
+# Display window
+cmds.showWindow(__WINDOW__)
